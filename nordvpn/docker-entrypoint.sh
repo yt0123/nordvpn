caf886e0b6fb265cb2817302d43a21a59793ebc2
Yusaku Takano <yt0123.dev@gmail.com>
2025-02-24 19:06:14 +0900

    Fixed docker-entrypoint.sh and nordvpnd.sh script

--- nordvpn/docker-entrypoint.sh
+++ nordvpn/docker-entrypoint.sh
@@ -5,22 +5,26 @@ set -eo pipefail
 # Run nordvpnd as daemon
 /nordvpnd.sh
 
-while [ ! -S /run/nordvpn/nordvpnd.sock ]; do
-    sleep 1
-done
+# Set Docker IPv4 subnet to whitelist
+nordvpn whitelist add subnet "$(/docker-ipv4.sh)" > /dev/null || {
+    echo "Docker IPv4 subnet already whitelisted."
+}
 
 # Disable analytics
-nordvpn set analytics disabled >/dev/null
-
-# Set Docker IPv4 subnet to whitelist
-nordvpn whitelist add subnet "$(/docker-ipv4.sh)" >/dev/null
+if [ "$(nordvpn settings | awk '/^Analytics:/ {print $2}')" = "enabled" ]; then
+    nordvpn set analytics disabled >/dev/null
+fi
 
 # Login automatically if token is present
 if [ -n "$NORDVPN_TOKEN" ]; then
-    nordvpn login --token "$NORDVPN_TOKEN" || {
-        echo "Invalid token."
-        exit 1
-    }
+    if nordvpn account >/dev/null; then
+        echo "Already logged in."
+    else
+        nordvpn login --token "$NORDVPN_TOKEN" || {
+            echo "Invalid token."
+            exit 1
+        }
+    fi
 fi
 
 # Run VPN automatically if connect option is present
--- nordvpn/nordvpnd.sh
+++ nordvpn/nordvpnd.sh
@@ -3,15 +3,31 @@
 set -euo pipefail
 
 SOCKET_DIR=/run/nordvpn
-PID_FILE=/run/nordvpn/nordvpn.pid
+PID_FILE=$SOCKET_DIR/nordvpn.pid
+SOCKET_FILE=$SOCKET_DIR/nordvpnd.sock
+
 LOG_FILE=/var/log/nordvpn/daemon.log
 
 if [ -f "$PID_FILE" ]; then
-    echo "Daemon already running."
-    exit 1
+    if kill -0 $(cat "$PID_FILE") 2>/dev/null; then
+        echo "Daemon already running."
+        exit 1
+    fi
+    # WARNING:
+    #   This condition implies that the daemon is not running but the PID file exists.
+    #   Run a daemon with new socket and overwrite the PID file.
+    if [ -S "$SOCKET_FILE" ]; then
+        unlink "$SOCKET_FILE"
+    fi
 fi
 
 mkdir -p "$SOCKET_DIR"
 
 nohup /usr/sbin/nordvpnd > "$LOG_FILE" 2>&1 &
 echo $! > "$PID_FILE"
+
+while [ ! -S "$SOCKET_FILE" ]; do
+    sleep 1
+done
+
+echo "NordVPN daemon started successfully."
